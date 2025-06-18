2b3aff8fb38c8939a1cdcd62bcb23a28d00f4c1d
sugan <suganraj.1211@gmail.com>
2025-06-18 18:40:20 +0530

    little improvements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![allow(unused_variables)]
+
+
 fn main() {
-    println!("Hello, world!");
+    let s:&str="Hello,World";
+    println!("the value of s is {s}");
+
+    let mut arr:[u32;8]=[0;8];
+    for i in 0..=7 {
+        arr[i]=i as u32;
+    }
+
+
+    for (i,&value) in arr.iter().enumerate(){
+        println!("The value in the {i}-th index is {value}");
+    }
+
 }
