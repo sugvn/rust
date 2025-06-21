9c68bea7ba0a09bd53c72696a21c9d5efd6b35e9
sugan <suganraj.1211@gmail.com>
2025-06-21 15:41:42 +0530

    onto the 5th chapter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_variables)]
+#![allow(dead_code)]
 
+use std::io;
 fn main() {
     // let s:&str="Hello,World";
     // println!("the value of s is {s}");
@@ -14,8 +16,6 @@ fn main() {
     //     println!("The value in the {i}-th index is {value}");
     // }
 
-
-    //testing mutability
     // let s1=String::from("Hello,World!");
     // println!("the value of s1 is {s1}");
     // let s2=s1.clone();
@@ -24,9 +24,9 @@ fn main() {
     // println!("the value of s2 is {s2}");
     // println!("Attempting to access s1 : {s1}");
 
-    fn chigga(name: &str) -> String {
-        return String::new();
-    }
+    // fn chigga(name: &str) -> String {
+    //     return String::new();
+    // }
 
     //testing with refernce
     // let s1=String::from("Open your ass Nisha");
@@ -35,16 +35,70 @@ fn main() {
     // println!("Trying to access again");
     // println!("The value of s1 is {s1} and the value of s2 is {s2} and the value of address of s1 is {} and s2 is {}",&s1,&s2);
 
-    fn fibonnaci(lvl: i32) -> bool {
-        println!("Hello from fib fn !");
-        true
-    }
+    // fn fibonnaci(lvl: i32) -> bool {
+    //     println!("Hello from fib fn !");
+    //     true
+    // }
     //testing with integers
     // let  mut x:[u32;5]=[0;5];
     // for i in 0..5 {
     //     x[i]=i as u32;
     // }
     // println!("successfully initialised x");
-    
+
+
+
+    //testing string slicing
+
+
+    // let mut inp_str=String::new();
+    // println!("Input a string:");
+    // io::stdin().read_line(&mut inp_str).expect("invalid input");
+    // fn first_word(sent:&str) -> &str {
+    //     let mut occurence:i32=0;
+    //     for i in sent.split_terminator(" "){
+    //         if i!=" "{
+    //             i
+    //         }
+    //     }
+    //     sent
+    // }
+    //
+
+
+                                                             //trailing spaces from a line
+    // let first_w=first_word(&mut inp_str);
+    // println!("{first_w}");
+    //
+
+    //slicing the first word from the sentence
+    // fn first_word(sentence:&String) -> &str {
+    //     let mut trimmed_str:&str=sentence.trim();
+    //     for i in 0..trimmed_str.len(){
+    //         if trimmed_str[i]==" " {
+    //             return &trimmed_str[0..i];
+    //         }
+    //     }
+    //     trimmed_str
+    // }
+
+
+    // fn first_word(sentence:&String)->&str{
+    //     let s=sentence.as_bytes();
+    //     for (i,&item) in s.iter().enumerate() {
+    //         if item==b' ' {
+    //             return &sentence[0..i];
+    //         }
+    //     }
+    //     &sentence[..]
+    // }
+    //
+
+
+
+
+
+
+
 
 }
